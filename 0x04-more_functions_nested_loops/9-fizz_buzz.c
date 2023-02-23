10f1811e73bb0734aa21acf8b099ddca10ab4b30
SherneVK <shernekinniar@gmail.com>
2023-02-23 10:59:15 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,35 +1,28 @@
-#include "main.h"
 #include <stdio.h>
 /**
- * main - prints a Fizz Buzz program
+ * main - prints numbers from 1 to 100
  * Return: 0
  */
+
 int main(void)
 {
 	int x;
+	char a[] = "Fizz";
+	char b[] = "Buzz";
+	char ab[] = "FizzBuzz";
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
-		{
-			printf("Fizzbuzz");
-		}
+		if (x == 100)
+			printf("%s ", b);
+		else if ((x % 3 == 0) && (x % 5 == 0))
+			printf("%s ", ab);
 		else if (x % 3 == 0)
-		{
-			printf("Fizz");
-		}
+			printf("%s ", a);
 		else if (x % 5 == 0)
-		{
-			printf("Buzz");
-		}
+			printf("%s ", b);
 		else
-		{
-			printf("%d", x);
-		}
-		if (x != 100)
-		{
-			printf(" ");
-		}
+			printf("%d ", x);
 	}
 	printf("\n");
 	return (0);
