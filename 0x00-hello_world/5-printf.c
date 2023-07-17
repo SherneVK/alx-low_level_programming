adf2ed3c877cb9152940f80729284ab68cd808e6
SherneVK <shernekinniar@gmail.com>
2023-07-17 10:52:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s, ", "with proper grammar but the outcome is a piece of art");
+	printf("\n");
+	return (0);
+}
+
