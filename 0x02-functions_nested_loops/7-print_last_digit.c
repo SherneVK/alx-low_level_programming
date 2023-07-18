8b65b97bf092ff1492dbcf25f9617f0be75a86b1
SherneVK <shernekinniar@gmail.com>
2023-07-18 10:15:10 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,12 @@
  */
 int print_last_digit(int number)
 {
+	/* Take the absolute value to handle negative numbers */
 	int num = (number >= 0) ? number : -number;
 
 	int last_digit = num % 10;
 
-	_putchar(last_digit + '0');
+	_putchar(last_digit + '0'); /* Convert the digit to a character and use _putchar to print it */
 
 	return (last_digit);
 }
