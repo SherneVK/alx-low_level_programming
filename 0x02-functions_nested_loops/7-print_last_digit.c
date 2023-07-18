6f541e9c83a8c0e73486288d6c25c2d95b4dd20c
SherneVK <shernekinniar@gmail.com>
2023-07-18 09:59:05 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,6 @@
 #include "main.h"
-#include <limits.h> /* Add this line to include <limits.h> for INT_MIN */
-#include <unistd.h> /* Add this line to include <unistd.h> for write */
-
-/**
- * _putchar - Dummy implementation to satisfy compilation requirements.
- * @c: The character to print.
- *
- * Return: On success, return the character.
- * On error, return -1 and errno is set appropriately.
- */
-int _putchar(char c)
-{
-    /* Your actual implementation of _putchar would go here */
-    return write(1, &c, 1);
-}
+#include <limits.h>
+#include <unistd.h>
 
 /**
  * print_last_digit - Prints the last digit of a number.
@@ -23,11 +10,25 @@ int _putchar(char c)
  */
 int print_last_digit(int number)
 {
-    /* Take the absolute value to handle negative numbers */
-    long int num = (number >= 0) ? number : -((long int)number);
+	/* Take the absolute value to handle negative numbers */
+	long int num = (number >= 0) ? number : -((long int)number);
 
-    int last_digit = num % 10;
-    _putchar(last_digit + '0'); /* Convert the digit to a character and use _putchar to print it */
+	int last_digit = num % 10;
 
-    return last_digit;
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int last_digit = print_last_digit(INT_MIN);
+
+	_putchar('\n');
+
+	return (0);
+
 }
