7658fcddfd0219b14ce9ef9cb393c7c318646231
SherneVK <shernekinniar@gmail.com>
2023-07-24 07:08:03 -0700

    123

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,16 +8,15 @@
  */
 void print_array(int *a, int n)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < n)
-    {
-        printf("%d", a[i]);
-        if (i != n - 1)
-            printf(", ");
-        i++;
-    }
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+		i++;
+	}
 
-    printf("\n");
+	printf("\n");
 }
-
