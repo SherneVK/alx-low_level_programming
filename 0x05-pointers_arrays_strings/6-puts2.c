4f9dc18fb797134ea8834103259dccca275cdd3e
SherneVK <shernekinniar@gmail.com>
2023-07-24 05:06:44 -0700

    123

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,31 @@
 #include "main.h"
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
+/**
  * puts2 - Prints every other character of a string, starting with the first character
  * @str: Pointer to the string to be printed
  */
 void puts2(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i += 2;
-	}
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i += 2;
+    }
 
-	_putchar('\n');
+    _putchar('\n');
 }
+
