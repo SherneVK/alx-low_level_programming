e4d3db47772b5c764bfc065e91c33644443e32d8
SherneVK <shernekinniar@gmail.com>
2023-07-24 07:23:37 -0700

    123

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 32
+
+/**
+ * generate_random_password - Generates a random valid password
+ *
+ * Return: Pointer to the generated password
+ */
+char *generate_random_password(void)
+{
+    static char password[PASSWORD_LENGTH + 1];
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int i;
+
+    srand(time(NULL)); // Seed the random number generator with the current time
+
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+
+    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password string
+
+    return password;
+}
+
+int main(void)
+{
+    char *password = generate_random_password();
+
+    printf("%s\n", password);
+
+    return 0;
+}
+
