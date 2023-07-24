78d5958cb121e2905af4aa94198193702ba8909d
SherneVK <shernekinniar@gmail.com>
2023-07-24 04:17:52 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: Pointer to the string to be printed
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
