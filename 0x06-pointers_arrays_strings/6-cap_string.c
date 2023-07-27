243974380ff266d716cf57da437d479d81dc20a1
SherneVK <shernekinniar@gmail.com>
2023-07-27 14:36:35 -0700

    123

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdbool.h>
-#include <string.h>
+
+#include <stdbool.h> // Required for using bool data type
 
 /**
- * is_separator - Check if a character is a separator.
+ * is_separator - Checks if a character is a word separator.
  * @c: The character to check.
  *
- * Return: true if the character is a separator, false otherwise.
+ * Return: true if the character is a separator, otherwise false.
  */
 bool is_separator(char c)
 {
@@ -20,22 +20,29 @@ bool is_separator(char c)
 }
 
 /**
- * cap_string - Capitalize the first character of each word in a string.
- * @str: The input string to capitalize.
+ * cap_string - Capitalizes all words of a string.
+ * @str: The input string.
  *
- * Return: Pointer to the modified input string `str`.
+ * Return: A pointer to the modified string.
  */
 char *cap_string(char *str)
 {
-    bool capitalize_next = true;
-    char *word = strtok(str, " \t\n,;.!?\"(){}");
+    bool capitalize_next = true; // Start with the first character capitalized
+    char *ptr = str;
 
-    while (word)
+    while (*ptr != '\0')
     {
-        if (capitalize_next)
-            *word = toupper(*word);
-        capitalize_next = false;
-        word = strtok(NULL, " \t\n,;.!?\"(){}");
+        if (is_separator(*ptr))
+            capitalize_next = true; // Capitalize the next character after a separator
+        else if (*ptr >= 'a' && *ptr <= 'z' && capitalize_next)
+        {
+            *ptr -= ('a' - 'A'); // Convert lowercase to uppercase
+            capitalize_next = false; // Word already capitalized
+        }
+        else
+            capitalize_next = false; // Word already capitalized or not a lowercase character
+
+        ptr++;
     }
 
     return str;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ char *cap_string(char *str);
 char *rot13(char *str);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+bool is_separator(char c);
 
 #endif /* MAIN_H */
