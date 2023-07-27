222f7b8a80eb28b0d9e891676415f654afd2f28d
SherneVK <shernekinniar@gmail.com>
2023-07-27 15:02:32 -0700

    123

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,6 @@
 #include "main.h"
 
 /**
- * is_separator - Check if a character is a separator.
- * @c: The character to check.
- *
- * Return: 1 if the character is a separator, 0 otherwise.
- */
-int is_separator(char c)
-{
-    char separators[] = " \t\n,;.!?\"(){}";
-    for (int i = 0; separators[i] != '\0'; i++)
-    {
-        if (c == separators[i])
-            return 1;
-    }
-    return 0;
-}
-
-/**
- * to_upper - Convert a lowercase character to uppercase.
- * @c: The character to convert.
- *
- * Return: The uppercase version of the character.
- */
-char to_upper(char c)
-{
-    return c >= 'a' && c <= 'z' ? c - ('a' - 'A') : c;
-}
-
-/**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
  *
@@ -36,28 +8,31 @@ char to_upper(char c)
  */
 char *cap_string(char *str)
 {
-    int index = 0;
-    int capitalize_next = 1;
+	int c = 0;
 
-    while (str[index])
-    {
-        if (is_separator(str[index]))
-        {
-            capitalize_next = 1;
-        }
-        else if (capitalize_next && str[index] >= 'a' && str[index] <= 'z')
-        {
-            str[index] = to_upper(str[index]);
-            capitalize_next = 0;
-        }
-        else
-        {
-            capitalize_next = 0;
-        }
+	while (str[c])
+	{
+		while (!(str[c] >= 'a' && str[c] <= 'z'))
+			index++;
 
-        index++;
-    }
+		if (str[c - 1] == ' ' ||
+		    str[c - 1] == '\t' ||
+		    str[c - 1] == '\n' ||
+		    str[c - 1] == ',' ||
+		    str[c - 1] == ';' ||
+		    str[c - 1] == '.' ||
+		    str[c - 1] == '!' ||
+		    str[c - 1] == '?' ||
+		    str[c - 1] == '"' ||
+		    str[c - 1] == '(' ||
+		    str[c - 1] == ')' ||
+		    str[c - 1] == '{' ||
+		    str[c - 1] == '}' ||
+		    c == 0)
+			str[c] -= 32;
 
-    return str;
+		c++;
+	}
+
+	return (str);
 }
-
