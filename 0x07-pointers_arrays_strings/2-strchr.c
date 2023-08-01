76f9ccaa76c9a83f2ba47c5d3f35d00d960f1525
SherneVK <shernekinniar@gmail.com>
2023-08-01 06:45:05 -0700

    123

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	for (; *s != '\0'; s++)
-	{
-		if (*s == c)
-			return (s);
-	}
+    while (*s != '\0')
+    {
+        if (*s == c)
+            return s;
+        s++;
+    }
 
-	if (*s == c)
-		return (s);
+    if (*s == c)
+        return s;
 
-	return (NULL);
+    return NULL;
 }
