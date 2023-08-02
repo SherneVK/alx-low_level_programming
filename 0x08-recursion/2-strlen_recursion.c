d02dccbd5dfdc71d888ed2d037ee06d35191681c
SherneVK <shernekinniar@gmail.com>
2023-08-02 10:49:00 -0700

    456

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,15 +3,13 @@
 /**
  * _strlen_recursion - Returns the length of a string.
  * @s: The string whose length is to be determined.
- *
+ * 
  * Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-
-	int restLength = _strlen_recursion(s + 1);
-
-	return (restLength + 1);
+    if (*s == '\0') // Base case: end of string
+        return 0;
+    
+    return _strlen_recursion(s + 1) + 1; // Recurse with the next character and then add 1
 }
