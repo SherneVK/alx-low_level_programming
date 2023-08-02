d2c1790d9dda8bbc81b9cdc4aca13bdc4e9c1a31
SherneVK <shernekinniar@gmail.com>
2023-08-02 12:01:39 -0700

    456

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,22 +8,24 @@
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-        return 0; // Not a prime if less than or equal to 1
+    int i;
+
+    if (n <= 1)
+        return 0; /* Not a prime if less than or equal to 1 */
 
     if (n <= 3)
-        return 1; // 2 and 3 are prime
+        return 1; /* 2 and 3 are prime */
 
     if (n % 2 == 0 || n % 3 == 0)
-        return 0; // Divisible by 2 or 3, not prime
+        return 0; /* Divisible by 2 or 3, not prime */
 
-    // Check divisibility by numbers of the form 6k ± 1
-    for (int i = 5; i * i <= n; i += 6)
+    /* Check divisibility by numbers of the form 6k ± 1 */
+    for (i = 5; i * i <= n; i += 6)
     {
         if (n % i == 0 || n % (i + 2) == 0)
-            return 0; // Divisible, not prime
+            return 0; /* Divisible, not prime */
     }
 
-    return 1; // If not divisible by any smaller primes, then it's prime
+    return 1; /* If not divisible by any smaller primes, then it's prime */
 }
 
