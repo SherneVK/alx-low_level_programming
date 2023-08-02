9f777120ea2d510ecd6d45e38b264a60e9f6481d
SherneVK <shernekinniar@gmail.com>
2023-08-02 12:44:21 -0700

    246

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,21 +13,23 @@ int wildcmp(char *s1, char *s2)
     if (*s2 == '*')
     {
         if (*(s2 + 1) == '\0')
-            return 1;
+            return 1; /* Wildcard matches the rest of s1 */
 
         if (*s1 == '\0')
-            return wildcmp(s1, s2 + 1);
+            return wildcmp(s1, s2 + 1); /* Continue checking after wildcard */
 
         return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
     }
     else if (*s1 == *s2)
     {
         if (*s1 == '\0')
-            return 1
+            return 1; /* Both strings are empty and matched so far */
 
-        return wildcmp(s1 + 1, s2 + 1);
+        return wildcmp(s1 + 1, s2 + 1); /* Continue comparing characters */
     }
     else
     {
-    return (0);
+        return 0; /* Characters don't match */
+    }
 }
+
