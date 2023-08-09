5a66c2601283f92a2138a8a33ea36d9447dbaf87
SherneVK <shernekinniar@gmail.com>
2023-08-08 17:56:45 -0700

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,11 +10,11 @@
  */
 void free_grid(int **grid, int height)
 {
-	if (grid == NULL || height <= 0)
-		return;
+	int i;
 
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
+	{
 		free(grid[i]);
-
+	}
 	free(grid);
 }
