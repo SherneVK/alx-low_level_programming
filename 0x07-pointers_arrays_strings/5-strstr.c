4908cf80873491fd814d3cce4390b4a0b9380a06
SherneVK <shernekinniar@gmail.com>
2023-08-09 14:58:40 -0700

    678

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,24 +11,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    char *h, *n;
+	char *h, *n;
 
-    while (*haystack)
-    {
-        h = haystack;
-        n = needle;
+	while (*haystack)
+	{
+		h = haystack;
+		n = needle;
 
-        while (*n && *h == *n)
-        {
-            h++;
-            n++;
-        }
+		while (*n && *h == *n)
+		{
+			h++;
+			n++;
+		}
 
-        if (*n == '\0')
-            return haystack;
+		if (*n == '\0')
+			return (haystack);
 
-        haystack++;
-    }
+		haystack++;
+	}
 
-    return NULL;
+	return (NULL);
 }
