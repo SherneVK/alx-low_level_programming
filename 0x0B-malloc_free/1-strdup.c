dcaefc50d91d06ac2f253e19e8a703bda4db140f
SherneVK <shernekinniar@gmail.com>
2023-08-08 17:27:07 -0700

    456

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,19 +10,20 @@
  */
 char *_strdup(char *str)
 {
-	char *duplicate;
-	unsigned int length = 0;
+	 char *duplicate;
+    unsigned int length = 0;
+    unsigned int i;
+    if (str == NULL)
+		return (NULL);
 
-	if (str == NULL)
-		return NULL;
-	while (str[length] != '\0')
+    while (str[length] != '\0')
         length++;
 
     duplicate = (char *)malloc((length + 1) * sizeof(char));
     if (duplicate == NULL)
         return NULL;
 
-    for (unsigned int i = 0; i <= length; i++)
+    for (i = 0; i <= length; i++)
         duplicate[i] = str[i];
 
     return duplicate;
