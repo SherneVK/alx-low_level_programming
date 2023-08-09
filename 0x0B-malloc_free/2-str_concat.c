a558485137e87d1a7e5ddd46bc9942a3aca0696b
SherneVK <shernekinniar@gmail.com>
2023-08-08 17:33:56 -0700

    123

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *str_concat(char *s1, char *s2)
+{
+    unsigned int len1 = 0, len2 = 0;
+    char *concatenated;
+
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    len1 = strlen(s1);
+    len2 = strlen(s2);
+
+    concatenated = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+    if (concatenated == NULL)
+        return NULL;
+
+    strcpy(concatenated, s1);
+    strcat(concatenated, s2);
+
+    return concatenated;
+}
+
