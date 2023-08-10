d9f5acbb0289543d49d681f9e670b13c8ff8c5d9
SherneVK <shernekinniar@gmail.com>
2023-08-10 03:51:57 -0700

    345

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,6 +14,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
     void *ptr;
     unsigned int total_size;
+    char *char_ptr;
+    unsigned int i;
 
     if (nmemb == 0 || size == 0)
         return (NULL);
@@ -25,9 +27,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
     if (ptr == NULL)
         return (NULL);
 
-    // Initialize the allocated memory to zero
-    char *char_ptr = (char *)ptr;
-    unsigned int i;
+    char_ptr = (char *)ptr;
 
     for (i = 0; i < total_size; i++)
         char_ptr[i] = 0;
