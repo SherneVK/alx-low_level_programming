4034f247e48c7b0ed093a401a1ade03e7b5ec11e
SherneVK <shernekinniar@gmail.com>
2023-08-14 12:29:11 -0700

    dog5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,30 +13,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog_ptr;
-    char *new_name, *new_owner;
+	dog_t *new_dog_ptr;
+	char *new_name, *new_owner;
 
-    new_dog_ptr = malloc(sizeof(dog_t));
-    if (new_dog_ptr == NULL)
-        return (NULL);
+	new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
+		return (NULL);
 
-    new_name = malloc(strlen(name) + 1);
-    new_owner = malloc(strlen(owner) + 1);
+	new_name = malloc(strlen(name) + 1);
+	new_owner = malloc(strlen(owner) + 1);
 
-    if (new_name == NULL || new_owner == NULL)
-    {
-        free(new_dog_ptr);
-        free(new_name);
-        free(new_owner);
-        return (NULL);
-    }
+	if (new_name == NULL || new_owner == NULL)
+	{
+		free(new_dog_ptr);
+		free(new_name);
+		free(new_owner);
+		return (NULL);
+	}
 
-    strcpy(new_name, name);
-    strcpy(new_owner, owner);
+	strcpy(new_name, name);
+	strcpy(new_owner, owner);
 
-    new_dog_ptr->name = new_name;
-    new_dog_ptr->age = age;
-    new_dog_ptr->owner = new_owner;
+	new_dog_ptr->name = new_name;
+	new_dog_ptr->age = age;
+	new_dog_ptr->owner = new_owner;
 
-    return (new_dog_ptr);
+	return (new_dog_ptr);
 }
