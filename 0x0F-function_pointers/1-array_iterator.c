5aada4e234bd2d21194cb5134b5c8599b8824fbc
SherneVK <shernekinniar@gmail.com>
2023-08-16 12:22:10 -0700

    777

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,12 +11,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (array != NULL && action != NULL)
-    {
-        for (size_t i = 0; i < size; i++)
-        {
-            action(array[i]);
-        }
-    }
-}
+	unsigned int e;
 
+	if (array != NULL && action != NULL)
+	{
+		for (size_t e = 0; e < size; e++)
+		{
+			action(array[e]);
+		}
+	}
+}
