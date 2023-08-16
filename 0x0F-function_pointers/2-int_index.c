211ec569326c98fd7afa4456e0ef0aab451874a4
SherneVK <shernekinniar@gmail.com>
2023-08-16 12:51:13 -0700

    456

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,16 +11,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	int i = 0;
-	while (i < size)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
-		i++;
+		for (int i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
 	}
-
 	return (-1);
 }
