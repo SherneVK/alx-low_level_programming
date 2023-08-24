c4cee7729ce7dcad04cee3c9195f6a2daa4f48f0
SherneVK <shernekinniar@gmail.com>
2023-08-24 14:54:46 -0700

    list len

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,4 +19,5 @@ typedef struct list_s
 /* Function to print all elements of a list_t list */
 size_t print_list(const list_t *h);
 int _putchar(char c);
+size_t list_len(const list_t *h);
 #endif /* LISTS_H */
